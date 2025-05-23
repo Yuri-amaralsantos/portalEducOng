805d94800e4c31480ba3a92d30fc1c499f1a6d23
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-05-23 12:26:09 -0300

    adicionando novo jogo

--- src/pages/jogos/dataList.js
+++ src/pages/jogos/dataList.js
@@ -4,6 +4,15 @@ export const jogos = [
         id: "mathgame",
         nome: "Jogo de Matemática",
         descricao: "Um jogo educativo para praticar contas de adição.",
+        imagem: "https://img.itch.zone/aW1nLzIxMDcxOTA1LnBuZw==/315x250%23c/osCLoq.png",
         embedUrl: "https://itch.io/embed-upload/13612178?color=333333",
+    },
+
+    {
+        id: "educQuest",
+        nome: "Jogo de Tabuleiro com questões",
+        descricao: "Um jogo de tabuleiro com foco em acerta questões variadas.",
+        imagem: "https://img.itch.zone/aW1nLzIxMzIzMjAwLnBuZw==/315x250%23c/s86IH7.png",
+        embedUrl: "https://itch.io/embed-upload/13777618?color=333333",
     },
 ];
--- src/pages/jogos/listaJogos.jsx
+++ src/pages/jogos/listaJogos.jsx
@@ -1,22 +1,26 @@
 // src/pages/Jogos.jsx
 import { jogos } from "./dataList";
 import { useNavigate } from "react-router-dom";
+import "./Jogos.css"; // Importe o CSS
 
 export default function JogosList() {
   const navigate = useNavigate();
 
   return (
-    <div className="container">
+    <div className="jogos-container">
       <h1>Lista de Jogos</h1>
-      <div className="card">
+      <div className="jogos-grid">
         {jogos.map((jogo) => (
           <div
+            className="jogo-card"
             key={jogo.id}
             onClick={() => navigate(`/jogos/${jogo.id}`)}
-            style={{ cursor: "pointer", marginBottom: "20px" }}
           >
-            <h3>{jogo.nome}</h3>
-            <p>{jogo.descricao}</p>
+            <img src={jogo.imagem} alt={jogo.nome} className="jogo-imagem" />
+            <div className="jogo-info">
+              <h3>{jogo.nome}</h3>
+              <p>{jogo.descricao}</p>
+            </div>
           </div>
         ))}
       </div>
