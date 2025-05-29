1da5ddf7e05ef96d310711fa6fff63a3ce0a5788
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-05-29 17:33:13 -0300

    atualizando sobre

--- src/pages/sobre/Sobre.jsx
+++ src/pages/sobre/Sobre.jsx
@@ -1,8 +1,26 @@
 export default function Sobre() {
   return (
     <div className="container">
-      <h2>Sobre a ong</h2>
+      <h2>Sobre</h2>
+      <p>
+        O portal Abrams Academy foi concebido com a finalidade de disponibilizar
+        cursos, jogos educativos e atividades avaliativas voltados ao
+        enriquecimento do conhecimento, em consonância com os princípios e
+        diretrizes do Instituto Abrams.
+      </p>
+      <hr></hr>
+      <a href="https://sites.google.com/view/institutoabrams/sobre-n%C3%B3s">
+        Site do instituto abrams
+      </a>
+      <hr></hr>
       <a href="/privacidade">Politica de privacidade</a>
+      <hr></hr>
+      <span>
+        Produzido por:{" "}
+        <a href="https://yuri-amaral-santos-portfolio.vercel.app">
+          Yuri amaral santos
+        </a>
+      </span>
     </div>
   );
 }
